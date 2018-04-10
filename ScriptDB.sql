96dac21de5d873ee437dfe6ba5e5b48f4b7d4464
Matheus Mendes <matheus.iba@icloud.com>
2018-04-10 16:56:03 -0300

    Update DBCreate

--- ScriptDB.sql
+++ ScriptDB.sql
@@ -1,17 +1,31 @@
-create database mercado;
-use mercado;
-create table produto(
-	codigoProduto int unique not null,
-	nomeProduto varchar(64) not null,
+create table if not exists produto(
+	codigoProduto bigint unique not null,
+	nomeProduto varchar(128) not null,
     qtdEstoque int,
     valorUnid double not null,
     primary key(codigoProduto)
 )engine=innodb;
-create table compras(
+create table if not exists compras(
 	codCompra int auto_increment not null unique,
     valorCompra double not null,
     dataCompra datetime,
     formaPagamento varchar(16) not null,
     caixaAtendente varchar(32) not null,
     primary key(codCompra)
+)engine=innodb;
+drop table cliente;
+create table if not exists cliente(
+	cpf varchar(13) not null unique,
+	nomeCliente varchar(32) not null,
+	primary key (cpf)
+) engine=innodb;
+drop table avaliacoes;
+create table if not exists avaliacoes(
+	idAvaliacao int auto_increment not null unique,
+	avaliacao int not null,
+    nomeCaixa varchar(32) not null,
+	cpf varchar(13) not null,
+	dataAvalia datetime not null,
+	foreign key(cpf) references cliente(cpf),
+    primary key(idAvaliacao)
 )engine=innodb;
